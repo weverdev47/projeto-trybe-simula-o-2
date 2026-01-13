8d12a407d52e24a8c912c1d6538440926dec2b49
weverdev47 <wevertonsantosdev47@gmail.com>
2026-01-13 00:37:59 -0300

    Cria estrutura inicial do projeto

---
+++ src/functions.js
@@ -0,0 +1,20 @@
+// Requisito 1
+function sum(a, b) {
+
+}
+
+// Requisito 2
+function isEven(number) {
+
+}
+
+// Requisito 3
+function maxNumber(numbers) {
+
+}
+
+module.exports = {
+  sum,
+  isEven,
+  maxNumber,
+};
